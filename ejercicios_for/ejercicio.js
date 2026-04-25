a8fe128ab0fbe678d02ca544fabe46f49ffcf640
academyjost <academyjost@gmail.com>
2026-04-24 20:09:39 -0500

    git add . git commit -m "agregamos funciones y codigo en html y js" git push

--- ejercicios_for/ejercicio.js
+++ ejercicios_for/ejercicio.js
@@ -1,1 +1,11 @@
-<DOCTYPE
+function listarNumeros(){
+    for(let i = 0; i < 3; i++){
+        console.log(i)
+    }
+}
+
+function ejecutar(numEjercicio){
+    if(numEjercicio == 1){
+        listarNumeros()
+    }
+}
